57fb9e6fefa03900b6922ea21c63cd4aefdf5344
ARTHUR GONÇALVES POLO <25.00376-5@maua.br>
2026-02-26 22:26:50 -0300

    imc_ideal

---
+++ vscode_python/algoritmos_ads1001/imc_ideal.py
@@ -0,0 +1,7 @@
+# Ler o sexo (masculino ou feminino) e a altura de uma pessoa em metros. Calcular o peso ideal :
+# masculino: pi = 72.7 x altura - 58
+# feminino: p1 = 62.1 x altura - 44.7
+
+#ENTRADAS
+sexo = input("Informe seu sexo: ")
+altura = float(input("Informe sua altura em metros: "))
